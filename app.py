9e767c8d2c3328d14b91c1eeaaaa4f0ce77765c4
Juan Alejandro <juan.calle.z@uniautonoma.edu.co>
2025-05-08 22:17:39 -0500

    Nuevas funcionalidades y nuevo diseno html

--- app.py
+++ app.py
@@ -1,22 +1,44 @@
 # importar bibliotecas
-from flask import Flask, render_template
+from flask import Flask, render_template,request,redirect,url_for
+from datetime import datetime
 
 # inicializar la aplicación
 app = Flask(__name__)
+
+#base de datos temporal
+materiales=[]
 
 # rutas
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/informacion1')
-def informacion1():
-    return render_template('informacion1.html')
+@app.route('/impacto')
+def impacto():
+    return render_template('impacto.html')
 
-@app.route('/informacion2')
-def informacion2():
-    return render_template('informacion2.html')
+@app.route('/estadisticas')
+def estadisticas():
+    return render_template('estadisticas.html')
+
+@app.route('/funciones')
+def funciones():
+    return render_template('funciones.html')
+
+@app.route('/reportes')
+def reportes():
+    return render_template('reportes.html')
+
+
+from werkzeug.utils import secure_filename
+import os
+
+
+
 
 # ejecutar la app
 if __name__ == "__main__":
     app.run(debug=True)  # facilidad para modificar el programa
+
+
+
